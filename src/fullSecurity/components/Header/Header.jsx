37050178e5f5ff9488a1c27fb34c4ff775ac741f
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-03-10 22:38:31 -0400

    changes

--- src/fullSecurity/components/Header/Header.jsx
+++ src/fullSecurity/components/Header/Header.jsx
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import '../../../../public/style/header/headerStyle.css';
 
 export const Header = () => {
 
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const image = new Image();
-    image.onload = () => setLoading(false);
-    image.src = "../../../src/assets/img/carrusel1.jpg";
-  }, []);
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
 
   console.log(loading)
 
   return (
     <>
+
       {loading && <div>Cargando...</div>}
-      <header>
-        <div className="eslogan animate__animated animate__bounceInLeft">
-          <h1>Seguridad al alcance de tus manos</h1>
-          <button>Más info</button>
-        </div>
-      </header>
+
+      <img
+          className="background-image"
+          src="../../../src/assets/img/carrusel1.jpg"
+          onLoad={handleImageLoad}
+          style={{display: 'none'}}
+        />
+    
+        <header>
+
+            <div className='eslogan animate__animated animate__bounceInLeft'>
+
+                <h1>Seguridad al alcance de tus manos</h1>
+
+                <button>Más info</button>
+
+            </div>
+
+        </header>
+    
     </>
   )
 }
